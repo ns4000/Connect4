3210155e960e70f3bfe5974d05ab7b92c3ae2bb5
ns4000 <ns40002@gmail.com>
2017-12-04 09:28:14 +0100

    frame and coins working state need work on win logic

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 C o n n e c t 4
 
 To run please first run the command:
--  npm install 
- then 
+-  npm install  
 -  gulp
--- app.js
+++ app.js
@@ -33,11 +33,14 @@ function switchPlayer(){
       CurrentPlayer =  "p1";
 };
 
+
+// need to add a if statment for row check if full
 // checking wheather a cell of the array is empty if so add a coin with a refrence to which player
 function checkIndex(col){
   if(indexArr[col][0].Player == null){
      indexArr[col][0].Player = CurrentPlayer;
      switchPlayer();
+  //   winCheck(col,0);
      return 0;// this return value is to determin which row the coin should go to
   }else if(indexArr[col][1].Player == null){
            indexArr[col][1].Player = CurrentPlayer;
@@ -65,6 +68,50 @@ function checkIndex(col){
      return 6;
   }
 };
+// winCheck should get the column and row and check if a horizantal, verticall or diagonal serise of 4 same value of current player occur then its should be a win
+function winCheck(col,row){
+ let youWon;
+  if (col < 4) {     //if we are on col 5 and above you cant win horizantally so no need to check
+    for (i = col; i <= col + 3; i++) {
+      console.log(col , row);
+      if (indexArr[i][row].Player == CurrentPlayer)
+        youWon += 1;
+      }
+    if (youWon == 4) {
+      // CURRENT PLAYER WON
+    } else {
+      youWon = 0; // reset the youWon value for further checking
+    }
+
+  //check if the win was verticlly, also if the row value is bigger than 3 no need to check for a win
+}else if (row < 3){
+   for(i = col ; i <= col + 4 ; i++ ){
+     if(indexArr[col][i].Player === CurrentPlayer)
+      youWon += 1 ;
+     }
+     if(youWon == 4 ){
+       //curent player won
+       alert('hi')
+     }else{
+       youWon = 0 ;
+     }
+
+  // win check for diagonal win
+}else if (col < 4 && row < 3) {
+  for (i = col; i <= col + 4; i++) {
+    if (indexArr[i][i].Player == CurrentPlayer) {
+      youWon += 1;
+    }
+  }
+  if(youWon == 4 ){
+    //curent player won
+  }else{
+    youWon = 0 ;
+   }
+ }// end of diagonal check
+
+};//end of winCheck()
+
 
 // click handler on the frame
 function onClick(e){
@@ -75,36 +122,35 @@ function onClick(e){
   }else
     return "images/orangePlayer.png";
   }
-  // creat an instance of sprite
+  // creat an instance of sprite depending on which player is on right now
   let Coin = PIXI.Sprite.fromImage(SpriteColor());
-  Coin.anchor.set(0.5);
+  Coin.anchor.set(0.5); //center the anchor for the Sprite
 
 
   // check which colume the click event happend with, if x is smaller than 80px then its the first column and so on
   let xx = e.data.global.x;
-  console.log(xx);
-  if(xx<80){
 
+  if(xx<80){
       Coin.x = 40;
-      Coin.y = 396-(checkIndex(0)*70); // after checking the index assign y value accordenling
+      Coin.y = 396-(checkIndex(0)*71); // after checking the index assign y value accordenling
     }else if(xx<150){
       Coin.x = 111;
-      Coin.y = 396-(checkIndex(1)*70);
+      Coin.y = 396-(checkIndex(1)*71);
     }else if(xx<220){
       Coin.x = 182;
-      Coin.y = 396-(checkIndex(2)*70);
+      Coin.y = 396-(checkIndex(2)*71);
     }else if(xx<290){
       Coin.x = 253;
-      Coin.y = 396-(checkIndex(3)*70);
+      Coin.y = 396-(checkIndex(3)*71);
     }else if(xx<370){
       Coin.x = 323;
-      Coin.y = 396-(checkIndex(4)*70);
+      Coin.y = 396-(checkIndex(4)*71);
     }else if(xx<430){
       Coin.x = 395;
-      Coin.y = 396-(checkIndex(5)*70);
+      Coin.y = 396-(checkIndex(5)*71);
     }else if(xx<550){
       Coin.x = 466;
-      Coin.y = 396-(checkIndex(6)*70);
+      Coin.y = 396-(checkIndex(6)*71);
     }
 // add the instance of the sprite
    app.stage.addChild(Coin);
